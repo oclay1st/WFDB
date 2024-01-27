8f2caba9b06d2460d3e9c16f888abf003a2da008
oclay <ocleidyreve@gmail.com>
2024-01-26 22:35:12 -0500

    Add utility for parsing numbers

---
+++ src/main/java/dev/oclay/wfdb/Util.java
@@ -0,0 +1,21 @@
+package dev.oclay.wfdb;
+
+public class Util {
+
+    private Util() {
+        throw new IllegalAccessError("Util class");
+    }
+
+    public static boolean isEmpty(String text) {
+        return text == null || text.isBlank();
+    }
+
+    public static int parseOrDefault(String text, int defaultValue) {
+        return isEmpty(text) ? defaultValue : Integer.parseInt(text);
+    }
+
+    public static float parseOrDefault(String text, float defaultValue) {
+        return isEmpty(text) ? defaultValue : Float.parseFloat(text);
+    }
+
+}
