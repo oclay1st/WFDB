f8c72ee4185016ce55479bdb6b927a46e8887ef2
oclay <ocleidyreve@gmail.com>
2024-01-28 01:10:38 -0500

    Rename the Header class as SingleSegmentHeader

---
+++ src/main/java/dev/oclay/wfdb/SingleSegmentHeader.java
@@ -0,0 +1,46 @@
+package dev.oclay.wfdb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public record SingleSegmentHeader(HeaderRecord headerRecord, HeaderSignal[] headerSignals) {
+
+    public static SingleSegmentHeader parse(InputStream input) throws IOException, ParseException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        HeaderRecord headerRecord = null;
+        HeaderSignal[] headerSignals = null;
+        int signalIndex = 0;
+        String headerLine;
+        boolean headerRecordProcessed = false;
+        while ((headerLine = reader.readLine()) != null) {
+            String stripedHeaderLine = headerLine.strip();
+            if (stripedHeaderLine.isEmpty() || stripedHeaderLine.charAt(0) == '#') {
+                continue;
+            }
+            if (!headerRecordProcessed) {
+                headerRecord = HeaderRecord.parse(stripedHeaderLine);
+                headerSignals = new HeaderSignal[headerRecord.numberOfSignals()];
+                headerRecordProcessed = true;
+            } else {
+                headerSignals[signalIndex] = HeaderSignal.parse(stripedHeaderLine);
+                signalIndex++;
+            }
+        }
+        return new SingleSegmentHeader(headerRecord, headerSignals);
+    }
+
+    public boolean isSingleFileFormat() {
+        return Arrays.stream(headerSignals)
+                .allMatch(headerSignal -> headerSignal.format() == headerSignals[0].format() &&
+                        headerSignal.filename().equals(headerSignals[0].filename()));
+    }
+
+    @Override
+    public String toString() {
+        return "SingleSegmentHeader [headerRecord = " + headerRecord + ", headerSignals = " + Arrays.toString(headerSignals) + "]";
+    }
+
+}
