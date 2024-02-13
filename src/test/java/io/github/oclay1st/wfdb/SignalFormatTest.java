5889dccf542cb928bb07c0896ce15b1c82dce6f8
oclay <ocleidyreve@gmail.com>
2024-02-12 21:54:08 -0500

    Refactor signal format tests

--- src/test/java/io/github/oclay1st/wfdb/SignalFormatTest.java
+++ src/test/java/io/github/oclay1st/wfdb/SignalFormatTest.java
@@ -1,151 +1,112 @@
 package io.github.oclay1st.wfdb;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import io.github.oclay1st.wfdb.mocks.MockHeaderSignal;
+
+import io.github.oclay1st.wfdb.formatters.SignalFormatter16;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter160;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter212;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter24;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter310;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter311;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter32;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter61;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter8;
+import io.github.oclay1st.wfdb.formatters.SignalFormatter80;
 
 class SignalFormatTest {
 
     @Test
-    @DisplayName("Should parse signal samples with format 8")
-    void shouldParseFormat8() {
-        byte[] source = { 0, -128, 127 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_8)
-                .initialValue(-2047)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_8.convertToSamples(source, headerSignals);
-        assertArrayEquals(new int[] { -2047, -2175, -2048 }, formattedSamples);
+    @DisplayName("Should parse the signal format 8")
+    void shouldMatchBitResolutionOfFormat8() {
+        SignalFormat format = SignalFormat.parse("8");
+        assertEquals(SignalFormat.FORMAT_8, format);
+        assertEquals(8, format.bitResolution());
+        assertInstanceOf(SignalFormatter8.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 16")
-    void shouldParseFormat16() {
-        byte[] source = { 1, 2, 3, 4 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_16)
-                .initialValue(513)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_16.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { 513, 1027 }, formattedSamples);
+    @DisplayName("Should parse the signal format 16")
+    void shouldMatchBitResolutionOfFormat16() {
+        SignalFormat format = SignalFormat.parse("16");
+        assertEquals(SignalFormat.FORMAT_16, format);
+        assertEquals(16, format.bitResolution());
+        assertInstanceOf(SignalFormatter16.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 24")
-    void shouldParseFormat24() {
-        byte[] source = { 1, 2, 3 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_24)
-                .initialValue(197121)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_24.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { 197121 }, formattedSamples);
+    @DisplayName("Should parse the signal format 24")
+    void shouldMatchBitResolutionOfFormat24() {
+        SignalFormat format = SignalFormat.parse("24");
+        assertEquals(SignalFormat.FORMAT_24, format);
+        assertEquals(24, format.bitResolution());
+        assertInstanceOf(SignalFormatter24.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 32")
-    void shouldParseFormat32() {
-        byte[] source = { 1, 2, 3, 4 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_32)
-                .initialValue(67305985)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_32.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { 67305985 }, formattedSamples);
+    @DisplayName("Should parse the signal format 32")
+    void shouldMatchBitResolutionOfFormat32() {
+        SignalFormat format = SignalFormat.parse("32");
+        assertEquals(SignalFormat.FORMAT_32, format);
+        assertEquals(32, format.bitResolution());
+        assertInstanceOf(SignalFormatter32.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 61")
-    void shouldParseFormat61() {
-        byte[] source = { 4, 3, 2, 1 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_61)
-                .initialValue(127)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_61.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { 1027, 513 }, formattedSamples);
+    @DisplayName("Should parse the signal format 61")
+    void shouldMatchBitResolutionOfFormat60() {
+        SignalFormat format = SignalFormat.parse("61");
+        assertEquals(SignalFormat.FORMAT_61, format);
+        assertEquals(16, format.bitResolution());
+        assertInstanceOf(SignalFormatter61.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 80")
-    void shouldParseFormat80() {
-        byte[] source = { 1, 2, 3, 4 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_80)
-                .initialValue(-127)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_80.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { -127, -126, -125, -124 }, formattedSamples);
+    @DisplayName("Should parse the signal format 80")
+    void shouldMatchBitResolutionOfFormat80() {
+        SignalFormat format = SignalFormat.parse("80");
+        assertEquals(SignalFormat.FORMAT_80, format);
+        assertEquals(8, format.bitResolution());
+        assertInstanceOf(SignalFormatter80.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 160")
-    void shouldParseFormat160() {
-        byte[] source = { 2, -128, 14, -126 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_160)
-                .initialValue(-65534)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_160.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { -65534, -65010 }, formattedSamples);
+    @DisplayName("Should parse the signal format 160")
+    void shouldMatchBitResolutionOfFormat160() {
+        SignalFormat format = SignalFormat.parse("160");
+        assertEquals(SignalFormat.FORMAT_160, format);
+        assertEquals(16, format.bitResolution());
+        assertInstanceOf(SignalFormatter160.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 212")
-    void shouldParseFormat212() {
-        byte[] source = { 1, 2, 3 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_212)
-                .initialValue(513)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_212.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { 513, 3 }, formattedSamples);
+    @DisplayName("Should parse the signal format 212")
+    void shouldMatchBitResolutionOfFormat212() {
+        SignalFormat format = SignalFormat.parse("212");
+        assertEquals(SignalFormat.FORMAT_212, format);
+        assertEquals(12, format.bitResolution());
+        assertInstanceOf(SignalFormatter212.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 310")
-    void shouldParseFormat310() {
-        byte[] source = { 1, 2, 3, 4 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_310)
-                .initialValue(256)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_310.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { 256, -511, 0 }, formattedSamples);
+    @DisplayName("Should parse the signal format 310")
+    void shouldMatchBitResolutionOfFormat310() {
+        SignalFormat format = SignalFormat.parse("310");
+        assertEquals(SignalFormat.FORMAT_310, format);
+        assertEquals(10, format.bitResolution());
+        assertInstanceOf(SignalFormatter310.class, format.formatter());
     }
 
     @Test
-    @DisplayName("Should parse signal samples with format 311")
-    void shouldParseFormat311() {
-        byte[] source = { 1, 2, 3, 4 };
-        HeaderSignal signal = new MockHeaderSignal.Builder()
-                .format(SignalFormat.FORMAT_311)
-                .initialValue(-511)
-                .build();
-        HeaderSignal[] headerSignals = { signal };
-        int[] formattedSamples = SignalFormat.FORMAT_311.convertToSamples(source, headerSignals);
-        assertNotNull(formattedSamples);
-        assertArrayEquals(new int[] { -511, 192, 64 }, formattedSamples);
+    @DisplayName("Should parse the signal format 311")
+    void shouldMatchBitResolutionOfFormat311() {
+        SignalFormat format = SignalFormat.parse("311");
+        assertEquals(SignalFormat.FORMAT_311, format);
+        assertEquals(10, format.bitResolution());
+        assertInstanceOf(SignalFormatter311.class, format.formatter());
     }
 
 }
