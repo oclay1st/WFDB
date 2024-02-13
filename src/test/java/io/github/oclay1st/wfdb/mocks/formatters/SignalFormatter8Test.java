8a42494ac0561f4302c307aa085720ec858d590e
oclay <ocleidyreve@gmail.com>
2024-02-12 22:52:39 -0500

    Test convertion of signal samples on format 8 to raw data

--- src/test/java/io/github/oclay1st/wfdb/mocks/formatters/SignalFormatter8Test.java
+++ src/test/java/io/github/oclay1st/wfdb/mocks/formatters/SignalFormatter8Test.java
@@ -12,18 +12,33 @@ import io.github.oclay1st.wfdb.formatters.SignalFormatter8;
 import io.github.oclay1st.wfdb.mocks.MockHeaderSignal;
 
 class SignalFormatter8Test {
- 
+
     @Test
-    @DisplayName("Should parse signal samples with format 8")
-    void shouldParseFormat8() {
+    @DisplayName("Should convert raw data to signal samples with format 8")
+    void shouldConvertFromRawDataToFormat8() {
         byte[] source = { 0, -128, 127 };
         HeaderSignal signal = new MockHeaderSignal.Builder()
                 .format(SignalFormat.FORMAT_8)
                 .initialValue(-2047)
                 .build();
         HeaderSignal[] headerSignals = { signal };
-        SignalFormatter formatter = new SignalFormatter8(); 
+        SignalFormatter formatter = new SignalFormatter8();
         int[] formattedSamples = formatter.convertBytesToSamples(source, headerSignals);
         assertArrayEquals(new int[] { -2047, -2175, -2048 }, formattedSamples);
     }
+
+    @Test
+    @DisplayName("Should convert signal samples with format 8 to raw data")
+    void shouldConvertFromFormat8ToRawData() {
+        int[] samples = { -2047, -2175, -2048 };
+        HeaderSignal signal = new MockHeaderSignal.Builder()
+                .format(SignalFormat.FORMAT_8)
+                .initialValue(-2047)
+                .build();
+        HeaderSignal[] headerSignals = { signal };
+        SignalFormatter formatter = new SignalFormatter8();
+        byte[] source = formatter.convertSamplesToBytes(samples, headerSignals);
+        assertArrayEquals(new byte[] { 0, -128, 127 }, source);
+    }
+
 }
