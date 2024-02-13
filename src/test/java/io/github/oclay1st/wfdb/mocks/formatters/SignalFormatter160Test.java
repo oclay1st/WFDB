3c4fc103b31a5d5f08224eeaefeb2db288114a3d
oclay <ocleidyreve@gmail.com>
2024-02-12 22:53:04 -0500

    Test convertion of signal samples on format 160 to raw data

--- src/test/java/io/github/oclay1st/wfdb/mocks/formatters/SignalFormatter160Test.java
+++ src/test/java/io/github/oclay1st/wfdb/mocks/formatters/SignalFormatter160Test.java
@@ -15,8 +15,8 @@ import io.github.oclay1st.wfdb.mocks.MockHeaderSignal;
 class SignalFormatter160Test {
 
     @Test
-    @DisplayName("Should parse signal samples with format 160")
-    void shouldParseFormat160() {
+    @DisplayName("Should convert raw data to signal samples with format 160")
+    void shouldConvertFromRawDataToFormat160() {
         byte[] source = { 2, -128, 14, -126 };
         HeaderSignal signal = new MockHeaderSignal.Builder()
                 .format(SignalFormat.FORMAT_160)
@@ -27,6 +27,21 @@ class SignalFormatter160Test {
         int[] formattedSamples = formatter.convertBytesToSamples(source, headerSignals);
         assertNotNull(formattedSamples);
         assertArrayEquals(new int[] { -65534, -65010 }, formattedSamples);
+    }
+
+    @Test
+    @DisplayName("Should convert signal samples with format 160 to raw data")
+    void shouldConvertFromFormat160ToRawData() {
+        int[] samples = { -65534, -65010 };
+        HeaderSignal signal = new MockHeaderSignal.Builder()
+                .format(SignalFormat.FORMAT_160)
+                .initialValue(-65534)
+                .build();
+        HeaderSignal[] headerSignals = { signal };
+        SignalFormatter formatter = new SignalFormatter160();
+        byte[] source = formatter.convertSamplesToBytes(samples, headerSignals);
+        assertNotNull(source);
+        assertArrayEquals(new byte[] { 2, -128, 14, -126 }, source);
     }
 
 }
