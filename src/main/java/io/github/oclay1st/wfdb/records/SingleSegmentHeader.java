06c283d22898ffa4d6fee0c3532adecfa0c89698
oclay <ocleidyreve@gmail.com>
2024-02-29 00:00:51 -0500

    Generate a checksum copy of the header

--- src/main/java/io/github/oclay1st/wfdb/records/SingleSegmentHeader.java
+++ src/main/java/io/github/oclay1st/wfdb/records/SingleSegmentHeader.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 import io.github.oclay1st.wfdb.exceptions.ParseException;
 
 /**
- * Represents a single segment record header
+ * Represents a single-segment record header.
  *
  * @param headerRecord  the header record
  * @param headerSignals the array of header signals
@@ -65,13 +65,49 @@ public record SingleSegmentHeader(HeaderRecord headerRecord, HeaderSignal[] head
     }
 
     /**
-     * Group the header signals by filename
+     * Group the header signals by filename.
      *
      * @return the group of header signals
      */
-    public Map<String, List<HeaderSignal>> groupByHeaderSignalsByFilename() {
+    public Map<String, List<HeaderSignal>> groupHeaderSignalsByFilename() {
         return Arrays.stream(headerSignals)
                 .collect(Collectors.groupingBy(HeaderSignal::filename, LinkedHashMap::new, Collectors.toList()));
+    }
+
+    /**
+     * Returns the single-segment header representation.
+     *
+     * @return the text block representation.
+     */
+    public String toTextBlock() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(headerRecord.toTextLine());
+        for (HeaderSignal headerSignal : headerSignals) {
+            builder.append("\n");
+            builder.append(headerSignal.toTextLine());
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Generate a copy of the header with the checksum of the singals.
+     *
+     * @param samplesPerSignal the array of samples per singal
+     * @return a new {@link SingleSegmentHeader} instance
+     */
+    public SingleSegmentHeader generateChecksumCopy(int[][] samplesPerSignal) {
+        HeaderSignal[] newHeaderSignals = new HeaderSignal[headerSignals.length];
+        for (int i = 0; i < headerSignals.length; i++) {
+            HeaderSignal headerSignal = headerSignals[i];
+            int initialValue = samplesPerSignal[i][0];
+            int checksum = HeaderSignal.calculateChecksum(samplesPerSignal[i]);
+            newHeaderSignals[i] = new HeaderSignal(headerSignal.filename(), headerSignal.format(),
+                    headerSignal.samplesPerFrame(), headerSignal.skew(), headerSignal.bytesOffset(),
+                    headerSignal.adcGain(), headerSignal.baseline(), headerSignal.unit(), headerSignal.adcResolution(),
+                    headerSignal.adcZero(), initialValue, checksum, headerSignal.blockSize(),
+                    headerSignal.description());
+        }
+        return new SingleSegmentHeader(headerRecord, newHeaderSignals);
     }
 
     @Override
