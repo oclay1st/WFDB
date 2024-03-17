95aa3c0d72229a239d742d858ff9ba563c932378
oclay <ocleidyreve@gmail.com>
2024-03-16 21:04:24 -0400

    Export multi-segment records

--- src/test/java/io/github/oclay1st/wfdb/records/MultiSegmentRecordTest.java
+++ src/test/java/io/github/oclay1st/wfdb/records/MultiSegmentRecordTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +21,18 @@ class MultiSegmentRecordTest {
         MultiSegmentRecord multiSegmentRecord = MultiSegmentRecord.parse(recordPath);
         assertNotNull(multiSegmentRecord);
         assertEquals(3, multiSegmentRecord.header().segments().size());
+    }
+
+    @Test
+    @DisplayName("Should export the multi-segment record")
+    void shouldExportTheMultiSegmentRecord() throws IOException, ParseException {
+        Path recordPath = Path.of("src", "test", "resources", "multi-segment", "v102s", "v102s");
+        MultiSegmentRecord multiSegmentRecord = MultiSegmentRecord.parse(recordPath);
+        assertNotNull(multiSegmentRecord);
+        Path exportedRecordPath = Files.createTempDirectory("export-multi-segment-record").resolve("v102s");
+        multiSegmentRecord.export(exportedRecordPath);
+        MultiSegmentRecord exportedRecord = MultiSegmentRecord.parse(exportedRecordPath);
+        assertEquals(multiSegmentRecord, exportedRecord);
     }
 
 }
